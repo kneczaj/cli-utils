d7ae24320ad136a2cf43df5ca653a801f039b425
Kamil Neczaj <kneczaj@gmail.com>
2025-06-21 14:23:56 +0200

    Add bash-git-prompt to source-all.sh

--- source-all.sh
+++ source-all.sh
@@ -2,3 +2,5 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "${SCRIPT_DIR}/bash/gentoo-prompt.sh"
+export GIT_PROMPT_ONLY_IN_REPO=1
+source "${SCRIPT_DIR}/bash/git-prompt/gitprompt.sh"
