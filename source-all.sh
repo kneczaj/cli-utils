815616da20295496d226173cdfba3768b8b38fd8
Kamil Neczaj <kneczaj@gmail.com>
2025-06-30 12:21:07 +0200

    Organize git tools to directories

--- source-all.sh
+++ source-all.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source "${SCRIPT_DIR}/bash/gentoo-prompt.sh"
+source "${SCRIPT_DIR}/bash/gentooPrompt.sh"
 export GIT_PROMPT_ONLY_IN_REPO=1
-source "${SCRIPT_DIR}/bash/git-prompt/gitprompt.sh"
-source "${SCRIPT_DIR}/git/git-aliases.sh"
+source "${SCRIPT_DIR}/bash/gitPrompt/gitprompt.sh"
+source "${SCRIPT_DIR}/git/gitAliases.sh"
+
+# git aliases overwrites SCRIPT_DIR
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/git/path.sh"
